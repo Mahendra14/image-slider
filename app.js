c0c22d71303209f12fb15dd38f65390fc2ad06a6
Mahendra Kumar <mahendra.raichur@gmail.com>
2021-10-30 21:51:57 +0530

    got all the var from html

---
+++ app.js
@@ -0,0 +1,5 @@
+//get all the required elements.
+const slider = document.querySelector(".slider");
+const container = document.querySelector(".container");
+const before = document.querySelector(".before");
+const after = document.querySelector(".after");
